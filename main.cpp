a8f642b252f64fcde9a364f687b9e54c07dfdef9
devilishwtr <odanowhome@gmail.com>
2017-05-11 08:29:15 +0700

    Created the second function - term caclulator

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@ int inputCheck (int term, int dep) {
     return 0;
 }
 
+int termCacl(int term, int dep){
+    float sum;
+    float bonus = 0;
+    if (dep > 10000) bonus = 0.01;
+    if (term >= 0 && term <= 30) sum = dep*0.9;
+    else if (term >= 31 && term <= 120 ) sum = dep * (1.02 + bonus);
+    else if (term >= 121 && term <= 240 ) sum = dep * (1.06 + bonus*2);
+    else if (term >= 241 && term <= 365 ) sum = dep * (1.12 + bonus*3);
+}
+
 int main() {
 
     return 0;
