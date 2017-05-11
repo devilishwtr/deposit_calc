7a917a4de7ef5b988bc7ceda8faf7e13c332f172
devilishwtr <odanowhome@gmail.com>
2017-05-11 09:00:37 +0700

    Wrote code in the main function and finished programm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+using namespace std;
 
 int inputCheck (int term, int dep) {
-    if (term <=0 || term > 365 || dep <=0) return 1;
+    if (term <=0 || term > 365 && dep <=0) return 1;
     return 0;
 }
 
@@ -13,9 +14,19 @@ int termCacl(int term, int dep){
     else if (term >= 31 && term <= 120 ) sum = dep * (1.02 + bonus);
     else if (term >= 121 && term <= 240 ) sum = dep * (1.06 + bonus*2);
     else if (term >= 241 && term <= 365 ) sum = dep * (1.12 + bonus*3);
+    return sum;
 }
 
 int main() {
-
+    int term, dep;
+    do {
+    cout << "Enter the deposit term\n";
+    cin >> term;
+    cout << "Enter the deposit amount\n";
+    cin >> dep;
+    } while (inputCheck(term,dep));
+    cout << "Total amount = "<< termCacl(term,dep) <<endl;
+    ;
+    system("pause");
     return 0;
 }
